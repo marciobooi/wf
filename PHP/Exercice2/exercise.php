e6623a4803b3a39b1abecfcea0aee393127c1643
marcio1978 <78marcio@gmail.com>
2019-02-25 12:23:38 +0100

    second exercise

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,14 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+
+
+
+
+$firstpart = substr($password, 0, floor(strlen($password)/2)+(strlen($password)%2));
+
+$secondpart = (substr($password, ceil(strlen($password)/2)));
+
+$saltedPassword = $firstpart.$salt.$secondpart;
+
+echo $saltedPassword;
