587c2c0d87596a210f5c33325aa324841b7e5436
marcio1978 <78marcio@gmail.com>
2019-02-26 11:00:22 +0100

    commit message

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,24 @@
 <?php
 
 $input;
-$winner;
+
+
+$winnerA = 0;
+$winnerB = 0;
+
+foreach ($input as $value)
+    if ($value[0]>$value[1]){
+        $winnerA++;
+    } else if ($value[0]<$value[1]){
+        $winnerB++;
+    }
+else{};
+
+if ($winnerA>$winnerB){
+    $winner = 'A';
+} else {
+    $winner = 'B';
+}
+
+
+
--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,20 @@
 <?php
+
+
+function getAllMondaysOfMonth ($year, $month){
+    $mondays = [];
+    $date = new DateTime('first monday of '.(DateTime::createFromFormat('Yn', $year.$month))->format('F Y'));
+
+    $interval = DateInterval::createFromDateString('next monday');
+    while($date->format('m') == $month) {
+        $mondays[] = $date->format('l j, M Y');
+        $date->add($interval);
+    }
+    return $mondays;
+
+
+}
+
+
+
+
